d1f313735563be54cc0d1608ec3cc054cd64c337
Margo-Katsan <3margo10@gmail.com>
2024-10-16 16:11:05 +0200

    Update ErrorPage.tsx

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,8 @@
+import { cn } from "@utils/cn";
+
 const ErrorPage = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className={cn('container', "flex h-screen items-center justify-center")}>
       <h1 className="text-4xl">Oops! Something went wrong.</h1>
       <p>The page you are looking for does not exist.</p>
     </div>

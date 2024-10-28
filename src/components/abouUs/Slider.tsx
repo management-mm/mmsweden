5743face4acd1cb2a868b3aaf7ba9b8d1ef004db
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2024-10-28 19:59:17 +0100

    Add photos to About Us page

--- src/components/abouUs/Slider.tsx
+++ src/components/abouUs/Slider.tsx
@@ -2,7 +2,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/scrollbar';
-import { Scrollbar } from 'swiper/modules';
+import { Scrollbar, Autoplay } from 'swiper/modules';
+import photosAboutUsList from '@constants/photosAboutUsList';
 
 const Slider = () => {
   return (
@@ -12,8 +13,12 @@ const Slider = () => {
       spaceBetween={'10px'}
       className="slider"
       loop={true}
-      modules={[Scrollbar]}
+      modules={[Scrollbar, Autoplay]}
       scrollbar={true}
+      autoplay={{
+        delay: 2000,
+          disableOnInteraction: true,
+        }}
       breakpoints={{
         768: {
           spaceBetween: 30,
@@ -22,10 +27,35 @@ const Slider = () => {
       centeredSlides={true}
       onSwiper={swiper => console.log(swiper)}
     >
-      {Array.from({ length: 6 }).map((_, index) => {
+      {photosAboutUsList.map((photo, index) => {
+        const {mobile, mobile2x, mobile3x, tablet, tablet2x, tablet3x, desktop, desktop2x, desktop3x, alt} = photo
         return (
           <SwiperSlide key={index * 3.45}>
-            <div className="block h-[200px] w-[330px] bg-slate-500 md:h-[362px] md:w-[580px] lg:h-[480px] lg:w-[754px]" />
+            <picture>
+            <source
+              srcSet={`${desktop} 1x, ${desktop2x} 2x, ${desktop3x} 3x`}
+              media="(min-width: 1178px)"
+              type="image/webp"
+            />
+
+            <source
+              srcSet={`${tablet} 1x, ${tablet2x} 2x, ${tablet3x} 3x`}
+              media="(min-width: 768px)"
+              type="image/webp"
+            />
+            <source
+              srcSet={`${mobile} 1x, ${mobile2x} 2x, ${mobile3x} 3x`}
+              media="(max-width: 767px)"
+              type="image/webp"
+            />
+            <img
+              src={`${mobile}`}
+              alt={alt}
+              width={330}
+              className="block rounded-[4px] h-[200px] w-[330px] bg-slate-500 md:h-[362px] md:w-[580px] lg:h-[480px] lg:w-[754px]"
+            />
+          </picture>
+            {/* <div className="block h-[200px] w-[290px] bg-slate-500 md:h-[362px] md:w-[580px] lg:h-[480px] lg:w-[754px]" /> */}
           </SwiperSlide>
         );
       })}
---
+++ src/constants/photosAboutUsList.ts
@@ -0,0 +1,83 @@
+import car from '@assets/images/about-us/car/index';
+import hampus from '@assets/images/about-us/hampus/index';
+import office from '@assets/images/about-us/office/index'
+import bilal from '@assets/images/about-us/bilal/index'
+import washing from '@assets/images/about-us/washing/index'
+import electrician from '@assets/images/about-us/electrician/index';
+
+const photosAboutUsList = [
+  {
+    mobile: hampus.mobile,
+    mobile2x: hampus.mobile2x,
+    mobile3x: hampus.mobile3x,
+    tablet: hampus.tablet,
+    tablet2x: hampus.tablet2x,
+    tablet3x: hampus.tablet3x,
+    desktop: hampus.desktop,
+    desktop2x: hampus.desktop2x,
+    desktop3x: hampus.desktop3x,
+    alt: 'Our seller'
+  },
+  {
+    mobile: electrician.mobile,
+    mobile2x: electrician.mobile2x,
+    mobile3x: electrician.mobile3x,
+    tablet: electrician.tablet,
+    tablet2x: electrician.tablet2x,
+    tablet3x: electrician.tablet3x,
+    desktop: electrician.desktop,
+    desktop2x: electrician.desktop2x,
+    desktop3x: electrician.desktop3x,
+    alt: 'Our elctrician'
+  },
+  {
+    mobile: office.mobile,
+    mobile2x: office.mobile2x,
+    mobile3x: office.mobile3x,
+    tablet: office.tablet,
+    tablet2x: office.tablet2x,
+    tablet3x: office.tablet3x,
+    desktop: office.desktop,
+    desktop2x: office.desktop2x,
+    desktop3x: office.desktop3x,
+    alt: 'Our office'
+  },
+  {
+    mobile: car.mobile,
+    mobile2x: car.mobile2x,
+    mobile3x: car.mobile3x,
+    tablet: car.tablet,
+    tablet2x: car.tablet2x,
+    tablet3x: car.tablet3x,
+    desktop: car.desktop,
+    desktop2x: car.desktop2x,
+    desktop3x: car.desktop3x,
+    alt: 'Machine loading'
+  },
+  {
+    mobile: washing.mobile,
+    mobile2x: washing.mobile2x,
+    mobile3x: washing.mobile3x,
+    tablet: washing.tablet,
+    tablet2x: washing.tablet2x,
+    tablet3x: washing.tablet3x,
+    desktop: washing.desktop,
+    desktop2x: washing.desktop2x,
+    desktop3x: washing.desktop3x,
+    alt: 'Machine washing'
+  },
+  {
+    mobile: bilal.mobile,
+    mobile2x: bilal.mobile2x,
+    mobile3x: bilal.mobile3x,
+    tablet: bilal.tablet,
+    tablet2x: bilal.tablet2x,
+    tablet3x: bilal.tablet3x,
+    desktop: bilal.desktop,
+    desktop2x: bilal.desktop2x,
+    desktop3x: bilal.desktop3x,
+    alt: 'Our mechanic'
+  }
+]
+
+export default photosAboutUsList

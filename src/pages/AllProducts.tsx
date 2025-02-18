0ee4d19912891efefa7ee04db7f64ba82c5a44d8
Margo-Katsan <3margo10@gmail.com>
2025-02-18 21:20:30 +0100

    Update AllProducts.tsx

--- src/pages/AllProducts.tsx
+++ src/pages/AllProducts.tsx
@@ -5,14 +5,17 @@ import Breadcrumb from '@components/common/Breadcrumb';
 import { useAuth } from '@hooks/useAuth';
 import useWindowWidth from '@hooks/useWindowWidth';
 
-import { cn } from '@utils/cn';
+import clsx from 'clsx';
 
 const AllProducts = () => {
   const { isLoggedIn } = useAuth();
   const windowWidth = useWindowWidth();
 
   return (
-    <div className={cn('container', 'pt-[12px] md:pt-[22px] lg:ml-0')}>
+    <div className={clsx('container',
+      'pt-[12px] md:pt-[22px]',
+      isLoggedIn && 'lg:ml-0'
+    )}>
       {!isLoggedIn && <Breadcrumb />}
       <div className="flex flex-col justify-between lg:flex-row lg:justify-start lg:gap-[30px]">
         {(!isLoggedIn || windowWidth < 1178) && <FiltersAndSearch />}

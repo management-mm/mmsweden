58cd02bf4cfca1eba6f742df049167af23c799ff
Margo-Katsan <3margo10@gmail.com>
2026-03-05 10:26:07 +0100

    feat: create MultiLanguageSchema for multilingual field validation

---
+++ schemas/multiLanguageSchema.ts
@@ -0,0 +1,11 @@
+import * as Yup from "yup";
+
+export const MultiLanguageSchema = Yup.object({
+  en: Yup.string().trim().required(),
+  de: Yup.string().trim().required(),
+  sv: Yup.string().trim().required(),
+  fr: Yup.string().trim().required(),
+  ru: Yup.string().trim().required(),
+  uk: Yup.string().trim().required(),
+  es: Yup.string().trim().required(),
+});

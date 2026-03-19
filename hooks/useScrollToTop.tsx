bf08d530e5d6c16aa48c5bbf8ef8a11a86556b05
Margo-Katsan <3margo10@gmail.com>
2026-03-19 09:03:53 +0100

    feat: add scroll-to-top hook and button

--- hooks/useScrollToTop.tsx
+++ hooks/useScrollToTop.tsx
@@ -4,12 +4,10 @@ import { useEffect } from 'react';
 
 import { usePathname } from 'next/navigation';
 
-export default function ScrollToTop() {
+export function useScrollToTop() {
   const pathname = usePathname();
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, [pathname]);
-
-  return null;
 }

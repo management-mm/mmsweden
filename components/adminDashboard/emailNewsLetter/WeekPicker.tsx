9563609de995fcdf3a3b7902f76fe5bf2b918ea1
Margo-Katsan <3margo10@gmail.com>
2026-03-26 14:07:41 +0100

    refactor: improve UI and layout for newsletter builder

--- components/adminDashboard/emailNewsLetter/WeekPicker.tsx
+++ components/adminDashboard/emailNewsLetter/WeekPicker.tsx
@@ -20,7 +20,7 @@ const WeekPicker: FC<IWeekPickerProps> = ({ setDateRangeText }) => {
   };
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex w-full flex-wrap items-center gap-2 md:flex-nowrap">
       <DatePicker
         label="From"
         value={startWeek}

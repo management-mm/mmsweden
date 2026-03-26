c67a532c631bd4f04fb4795363057ae4437e294a
Margo-Katsan <3margo10@gmail.com>
2026-03-26 13:14:45 +0100

    feat: improve SEO with SSR and server-side rendering

---
+++ components/productDetails/RecommendedProductsCarousel.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import type { IProduct } from '@interfaces/IProduct';
+import clsx from 'clsx';
+
+import DecorativeLine from '@components/common/DecorativeLine';
+import NaviArrowSlider from '@components/common/NaviArrowSlider';
+import ProductCard from '@components/common/productCard/ProductCard';
+
+import useSwiperNavigation from '@hooks/useSwiperNavigation';
+
+import type { AppLocale } from '@i18n/config';
+
+type Props = {
+  products: IProduct[];
+  language: AppLocale;
+  title: string;
+};
+
+const RecommendedProductsCarousel = ({ products, language, title }: Props) => {
+  const { handlePrev, handleNext, onSwiperInit } = useSwiperNavigation();
+
+  return (
+    <section aria-labelledby="recommended-products-heading">
+      <div className="container">
+        <div className="relative mb-[22px] flex flex-wrap items-end justify-between gap-[12px] md:flex-nowrap">
+          <h2
+            id="recommended-products-heading"
+            className="shrink-0 text-[18px] font-semibold md:text-[24px] md:leading-[0.8]"
+          >
+            {title}
+          </h2>
+
+          <DecorativeLine className={clsx()} intent="latestArrivals" />
+
+          <div className="hidden gap-[12px] md:flex">
+            <NaviArrowSlider onClick={handlePrev} iconId="ArrowLeft" />
+            <NaviArrowSlider onClick={handleNext} iconId="ArrowRight" />
+          </div>
+        </div>
+
+        <Swiper
+          onSwiper={onSwiperInit}
+          style={{ width: 'calc(49% + 50vw)' }}
+          slidesPerView="auto"
+          spaceBetween={10}
+          className="slider"
+          breakpoints={{
+            768: { spaceBetween: 30 },
+          }}
+        >
+          {products.map(product => (
+            <SwiperSlide key={product._id}>
+              <ProductCard
+                language={language}
+                product={product}
+                className="w-[296px] md:w-[264px]"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default RecommendedProductsCarousel;
